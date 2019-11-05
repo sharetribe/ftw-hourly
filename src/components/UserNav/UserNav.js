6929d9bb736a00968abd14618493b3291f72db1d
Jenni Nurmi <jenni@sharetribe.com>
2019-11-05 16:42:58 +0200

    Use OwnListingLink in UserNav

--- src/components/UserNav/UserNav.js
+++ src/components/UserNav/UserNav.js
@@ -4,23 +4,57 @@ import { FormattedMessage } from '../../util/reactIntl';
 import classNames from 'classnames';
 import { ACCOUNT_SETTINGS_PAGES } from '../../routeConfiguration';
 import { LinkTabNavHorizontal } from '../../components';
+import { ensureOwnListing } from '../../util/data';
+import { LISTING_STATE_DRAFT } from '../../util/types';
+import {
+  LISTING_PAGE_PARAM_TYPE_DRAFT,
+  LISTING_PAGE_PARAM_TYPE_EDIT,
+  createSlug,
+} from '../../util/urlHelpers';
 
 import css from './UserNav.css';
 
+const listingTab = (listing, selectedPageName) => {
+  if (!listing) {
+    return {
+      text: <FormattedMessage id="UserNav.newListing" />,
+      selected: selectedPageName === 'NewListingPage',
+      linkProps: {
+        name: 'NewListingPage',
+      },
+    };
+  }
+  const currentListing = ensureOwnListing(listing);
+  const id = currentListing.id.uuid;
+  const { title = '', state } = currentListing.attributes;
+  const slug = createSlug(title);
+  const isDraft = state === LISTING_STATE_DRAFT;
+
+  return {
+    text: <FormattedMessage id="UserNav.editListing" />,
+    selected: selectedPageName === 'EditListingPage',
+    linkProps: {
+      name: 'EditListingPage',
+      params: {
+        id,
+        slug,
+        type: isDraft ? LISTING_PAGE_PARAM_TYPE_DRAFT : LISTING_PAGE_PARAM_TYPE_EDIT,
+        tab: 'photos',
+      },
+    },
+  };
+};
+
 const UserNav = props => {
-  const { className, rootClassName, selectedPageName } = props;
+  const { className, rootClassName, selectedPageName, listing } = props;
   const classes = classNames(rootClassName || css.root, className);
 
   const tabs = [
     {
-      text: <FormattedMessage id="ManageListingsPage.yourListings" />,
-      selected: selectedPageName === 'ManageListingsPage',
-      linkProps: {
-        name: 'ManageListingsPage',
-      },
+      ...listingTab(listing, selectedPageName),
     },
     {
-      text: <FormattedMessage id="ManageListingsPage.profileSettings" />,
+      text: <FormattedMessage id="UserNav.profileSettingsPage" />,
       selected: selectedPageName === 'ProfileSettingsPage',
       disabled: false,
       linkProps: {
@@ -28,7 +62,7 @@ const UserNav = props => {
       },
     },
     {
-      text: <FormattedMessage id="ManageListingsPage.accountSettings" />,
+      text: <FormattedMessage id="UserNav.contactDetailsPage" />,
       selected: ACCOUNT_SETTINGS_PAGES.includes(selectedPageName),
       disabled: false,
       linkProps: {
