0fc02d05cdb0f416622b841cea8b0064ea34c38c
Jenni Nurmi <jenni@sharetribe.com>
2019-11-18 11:52:38 +0200

    Make Avatar bigger

--- src/containers/ListingPage/SectionAvatar.js
+++ src/containers/ListingPage/SectionAvatar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { NamedLink, AvatarLarge, AvatarMedium } from '../../components';
+import { AvatarLarge, AvatarMedium } from '../../components';
 
 import css from './ListingPage.css';
 
 const SectionAvatar = props => {
-  const { user, params } = props;
+  const { user } = props;
   return (
     <div className={css.sectionAvatar}>
-      <NamedLink name="ListingPage" params={params} to={{ hash: '#host' }}>
-        <AvatarLarge user={user} className={css.avatarDesktop} disableProfileLink />
-      </NamedLink>
-      <NamedLink name="ListingPage" params={params} to={{ hash: '#host' }}>
-        <AvatarMedium user={user} className={css.avatarMobile} disableProfileLink />
-      </NamedLink>
+      <AvatarLarge
+        user={user}
+        className={css.avatarDesktop}
+        initialsClassName={css.initialsDesktop}
+        disableProfileLink
+      />
+
+      <AvatarMedium user={user} className={css.avatarMobile} disableProfileLink />
     </div>
   );
 };
