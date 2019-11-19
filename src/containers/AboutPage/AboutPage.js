3f1f108b9b6e9abcd8c9a89e2984bfb13cb37343
Vesa Luusua <vesa.luusua@gmail.com>
2019-11-19 17:15:11 +0200

    Changes to about page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -25,7 +25,7 @@ const AboutPage = () => {
       schema={{
         '@context': 'http://schema.org',
         '@type': 'AboutPage',
-        description: 'About Saunatime',
+        description: 'About Yogatime',
         name: 'About page',
       }}
     >
@@ -35,48 +35,46 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <h1 className={css.pageTitle}>Find new depths in your yoga practice</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+              <p>Yoga was listed by UNESCO as an intangible cultural heritage.</p>
             </div>
 
             <div className={css.contentMain}>
               <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
+                Each yoga practitioner is an individual, and each one of us needs different care.
+                Working together with an experienced yoga teacher offers the possibility to rise
+                our practise to a whole new level.
               </h2>
 
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                Whether it is the alignment of asanas or being able to set the focus of the class,
+                we all have our own struggles and goals. Some of these cannot be addressed in a
+                regular class of twenty yogis. Working together with the experienced yoga teachers
+                from Yogatime, you can together create just the right class for you.
               </p>
 
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
+              <h3 className={css.subtitle}>Are you a yoga teacher?</h3>
 
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                Yogatime offers you a platform through which you can reach thousands of yoga
+                practitioners. Offering private yoga classes through Yogatime offers you a
+                possibility to grow your customer base and earn some extra income on top of your
+                regular classes.
               </p>
 
               <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
+                Create your own marketplace like Yogatime
               </h3>
               <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                Yogatime is brought to you by{' '}
+                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Sharetribe
+                offers anyone a possibility to create a marketplace without restricting your own
+                creativity. Do not hesitate to reach out and learn how to best turn your
+                marketplace idea to reality.
               </p>
               <p>
                 You can also checkout our{' '}
