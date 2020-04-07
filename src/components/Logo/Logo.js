b5080b59c89a3fc5524b8ea40333252d3f2acd77
trdtru <63155018+trdtru@users.noreply.github.com>
2020-04-07 15:06:30 +0100

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+
+import config from '../../config';
 import IconLogo from './IconLogo';
+import LogoImage from './vivify-logo.png';
 import css from './Logo.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
   const mobileClasses = classNames(css.logoMobile, className);
 
-  // If you want to use image instead of svg as a logo you can use the following code.
-  // Also, remember to import the image as LogoImage here.
-  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+  if (format === 'desktop') {
+    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+  }
 
-  return (
-    <IconLogo
-      className={format === 'desktop' ? className : mobileClasses}
-      format={format}
-      {...rest}
-    />
-  );
+  return <IconLogo className={mobileClasses} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
