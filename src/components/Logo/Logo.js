eeb004dbd078b973e16e9a837df4ef365ca65afa
trdtru <63155018+trdtru@users.noreply.github.com>
2020-04-07 17:00:30 +0100

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import config from '../../config';
-import IconLogo from './IconLogo';
+import IconLogo from './vivify-logo.png';
 import LogoImage from './vivify-logo.png';
 import css from './Logo.css';
 
