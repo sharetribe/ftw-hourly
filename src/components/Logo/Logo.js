ef9c2b94cedd159280063f8a9c51e71369487251
Noah Talerman <noahtal@umich.edu>
2020-04-16 12:12:50 -0400

    Revert "changed logo"
    This reverts commit 24161e0dd7ddebed4518bb0cda2593b99c73119f.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import config from '../../config';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import LogoImage from './saunatime-logo.png';
+import IconLogo from './IconLogo';
 import css from './Logo.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
   const mobileClasses = classNames(css.logoMobile, className);
 
+  // If you want to use image instead of svg as a logo you can use the following code.
+  // Also, remember to import the image as LogoImage here.
+  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+
   return (
-    <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+    <IconLogo
+      className={format === 'desktop' ? className : mobileClasses}
+      format={format}
+      {...rest}
+    />
   );
 };
 
