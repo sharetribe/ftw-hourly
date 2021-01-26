c179e06cf64affc2ce5b32a41910d57c0e8c9cc9
khaimeister <k.duong@icloud.com>
2021-01-26 19:27:00 +0700

    Add modified 26/01/2021

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -118,11 +118,26 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=New%20York%20City%2C%20New%20York%2C%20USA&bounds=40.917576401307%2C-73.7008392055224%2C40.477399%2C-74.2590879797556',
+                        '?keywords=Health%20Practitioners',
+                      // '?address=New%20York%20City%2C%20New%20York%2C%20USA&bounds=40.917576401307%2C-73.7008392055224%2C40.477399%2C-74.2590879797556',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchNewYork" />
+                    <FormattedMessage id="Footer.searchHealth" />
+                  </NamedLink>
+                </li>
+
+                <li className={css.listItem}>
+                  <NamedLink
+                    name="SearchPage"
+                    to={{
+                      search:
+                        '?keywords=IT%20&%20Technology%20Gurus',
+                      // '?address=San%20Francisco%2C%20California%2C%20USA&bounds=37.8324430069081%2C-122.354995082683%2C37.6044780500533%2C-122.517910874663',
+                    }}
+                    className={css.link}
+                  >
+                    <FormattedMessage id="Footer.searchIT" />
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
@@ -130,11 +145,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=Los%20Angeles%2C%20California%2C%20USA&bounds=34.161440999758%2C-118.121305008073%2C33.9018913203336%2C-118.521456965901',
+                        '?keywords=Sports%20&%20Fitness%20Instructors',
+                      // '?address=Seattle%2C%20Washington%2C%20USA&bounds=47.7779392908564%2C-122.216605992108%2C47.3403950185547%2C-122.441233019046',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchLosAngeles" />
+                    <FormattedMessage id="Footer.searchSports" />
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
@@ -142,35 +158,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=San%20Francisco%2C%20California%2C%20USA&bounds=37.8324430069081%2C-122.354995082683%2C37.6044780500533%2C-122.517910874663',
+                        '?keywords=Accountants%20&%20Financial%20Advisors',
+                      // '?address=Portland%2C%20Oregon%2C%20USA&bounds=45.858099013046%2C-122.441059986416%2C45.3794799927623%2C-122.929215816001',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchSanFrancisco" />
-                  </NamedLink>
-                </li>
-                <li className={css.listItem}>
-                  <NamedLink
-                    name="SearchPage"
-                    to={{
-                      search:
-                        '?address=Seattle%2C%20Washington%2C%20USA&bounds=47.7779392908564%2C-122.216605992108%2C47.3403950185547%2C-122.441233019046',
-                    }}
-                    className={css.link}
-                  >
-                    <FormattedMessage id="Footer.searchSeattle" />
-                  </NamedLink>
-                </li>
-                <li className={css.listItem}>
-                  <NamedLink
-                    name="SearchPage"
-                    to={{
-                      search:
-                        '?address=Portland%2C%20Oregon%2C%20USA&bounds=45.858099013046%2C-122.441059986416%2C45.3794799927623%2C-122.929215816001',
-                    }}
-                    className={css.link}
-                  >
-                    <FormattedMessage id="Footer.searchPortland" />
+                    <FormattedMessage id="Footer.searchAccountants" />
                   </NamedLink>
                 </li>
               </ul>
@@ -182,11 +175,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=Denver%2C%20Colorado%2C%20United%20States%20of%20America&bounds=39.94623402%2C-104.600299056%2C39.62371698%2C-105.193616003506',
+                        '?keywords=Legal%20Experts',
+                      // '?address=Los%20Angeles%2C%20California%2C%20USA&bounds=34.161440999758%2C-118.121305008073%2C33.9018913203336%2C-118.521456965901',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchDenver" />
+                    <FormattedMessage id="Footer.searchLegal" />
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
@@ -194,11 +188,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=Philadelphia%2C%20Pennsylvania%2C%20United%20States%20of%20America&bounds=40.1379937851305%2C-74.9557749984862%2C39.8557310196928%2C-75.2946589071447',
+                        '?keywords=Educators%20&%20Teachers',
+                      // '?address=Philadelphia%2C%20Pennsylvania%2C%20United%20States%20of%20America&bounds=40.1379937851305%2C-74.9557749984862%2C39.8557310196928%2C-75.2946589071447',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchPhiladelphia" />
+                    <FormattedMessage id="Footer.searchEducators" />
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
@@ -206,11 +201,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=Boston%2C%20Massachusetts%2C%20United%20States%20of%20America&bounds=42.3974009328397%2C-70.9860500028801%2C42.3196059806256%2C-71.1255750165112',
+                        '?keywords=Fashionistas%20&%20Beauticians',
+                      // '?address=Denver%2C%20Colorado%2C%20United%20States%20of%20America&bounds=39.94623402%2C-104.600299056%2C39.62371698%2C-105.193616003506',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchBoston" />
+                    <FormattedMessage id="Footer.searchFashionistas" />
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
@@ -218,23 +214,12 @@ const Footer = props => {
                     name="SearchPage"
                     to={{
                       search:
-                        '?address=San%20Diego%2C%20California%2C%20United%20States%20of%20America&bounds=33.0722089336828%2C-116.853118984%2C32.534171982%2C-117.266223298428',
+                        '?keywords=Home%20Improvement%20&%20Housekeeping',
+                      // '?address=Boston%2C%20Massachusetts%2C%20United%20States%20of%20America&bounds=42.3974009328397%2C-70.9860500028801%2C42.3196059806256%2C-71.1255750165112',
                     }}
                     className={css.link}
                   >
-                    <FormattedMessage id="Footer.searchSanDiego" />
-                  </NamedLink>
-                </li>
-                <li className={css.listItem}>
-                  <NamedLink
-                    name="SearchPage"
-                    to={{
-                      search:
-                        '?address=Boulder%2C%20Colorado%2C%20United%20States%20of%20America&bounds=40.1593785009969%2C-105.108872052936%2C39.9139839802231%2C-105.525489934809',
-                    }}
-                    className={css.link}
-                  >
-                    <FormattedMessage id="Footer.searchBoulder" />
+                    <FormattedMessage id="Footer.searchHome" />
                   </NamedLink>
                 </li>
               </ul>
