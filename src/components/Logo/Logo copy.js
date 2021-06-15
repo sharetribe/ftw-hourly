9787ed2425f20cf865c84b4f19e64d7ffae57c78
brycebowman <Bryceabowman@gmail.com>
2021-06-15 09:17:40 -0500

    added logo height

---
+++ src/components/Logo/Logo copy.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import IconLogo from './IconLogo';
+import css from './Logo.module.css';
+
+const Logo = props => {
+  const { className, format, ...rest } = props;
+  const mobileClasses = classNames(css.logoMobile, className);
+
+  // If you want to use image instead of svg as a logo you can use the following code.
+  // Also, remember to import the image as LogoImage here.
+  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+
+  return (
+    <IconLogo
+      className={format === 'desktop' ? className : mobileClasses}
+      format={format}
+      {...rest}
+    />
+  );
+};
+
+const { oneOf, string } = PropTypes;
+
+Logo.defaultProps = {
+  className: null,
+  format: 'desktop',
+};
+
+Logo.propTypes = {
+  className: string,
+  format: oneOf(['desktop', 'mobile']),
+};
+
+export default Logo;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { oneOf, string } from 'prop-types';
 import classNames from 'classnames';
-import IconLogo from './IconLogo';
+
+import config from '../../config';
+import MobileLogoImage from './cafetemps-logo-small.png';
+import DesktopLogoImage from './cafetemps-logo.png';
 import css from './Logo.module.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
-  const mobileClasses = classNames(css.logoMobile, className);
-
-  // If you want to use image instead of svg as a logo you can use the following code.
-  // Also, remember to import the image as LogoImage here.
-  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+  const isMobile = format !== 'desktop';
+  const classes = classNames(className, { [css.logoMobile]: isMobile });
+  const logoImage = isMobile ? MobileLogoImage : DesktopLogoImage;
 
   return (
-    <IconLogo
-      className={format === 'desktop' ? className : mobileClasses}
-      format={format}
+    <img
+      className={classes}
+      src={logoImage}
+      alt={config.siteTitle}
       {...rest}
     />
   );
 };
-
-const { oneOf, string } = PropTypes;
 
 Logo.defaultProps = {
   className: null,
