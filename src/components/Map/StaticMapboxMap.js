0f2718ee239070849a6086be4dcbe7fa2b83d77a
brycebowman <Bryceabowman@gmail.com>
2021-06-22 14:29:25 -0500

    fix map error message

--- src/components/Map/StaticMapboxMap.js
+++ src/components/Map/StaticMapboxMap.js
@@ -53,7 +53,7 @@ const StaticMapboxMap = props => {
 
   const overlay = mapOverlay(center, mapsConfig);
   const src =
-    'https://api.mapbox.com/styles/v1/mapbox/light-v10/static' +
+    'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static' +
     (overlay ? `/${overlay}` : '') +
     `/${center.lng},${center.lat},${zoom}` +
     `/${width}x${height}` +
@@ -61,67 +61,7 @@ const StaticMapboxMap = props => {
 
   return <img src={src} alt={address} />;
 };
-map.on('load', function () {
-// Insert the layer beneath any symbol layer.
-var layers = map.getStyle().layers;
-var labelLayerId;
-for (var i = 0; i < layers.length; i++) {
-if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-labelLayerId = layers[i].id;
-break;
-}
-}
-var map = new mapboxgl.Map({
-style: 'mapbox://styles/mapbox/light-v10',
-center: [-74.0066, 40.7135],
-zoom: 15.5,
-pitch: 45,
-bearing: -17.6,
-container: 'map',
-antialias: true
-});
-// The 'building' layer in the Mapbox Streets
-// vector tileset contains building height data
-// from OpenStreetMap.
-map.addLayer(
-{
-'id': 'add-3d-buildings',
-'source': 'composite',
-'source-layer': 'building',
-'filter': ['==', 'extrude', 'true'],
-'type': 'fill-extrusion',
-'minzoom': 15,
-'paint': {
-'fill-extrusion-color': '#aaa',
 
-// Use an 'interpolate' expression to
-// add a smooth transition effect to
-// the buildings as the user zooms in.
-'fill-extrusion-height': [
-'interpolate',
-['linear'],
-['zoom'],
-15,
-0,
-15.05,
-['get', 'height']
-],
-'fill-extrusion-base': [
-'interpolate',
-['linear'],
-['zoom'],
-15,
-0,
-15.05,
-['get', 'min_height']
-],
-'fill-extrusion-opacity': 0.6
-}
-},
-
-labelLayerId
-);
-});
 StaticMapboxMap.defaultProps = {
   address: '',
   center: null,
