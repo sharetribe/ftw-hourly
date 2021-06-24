2a5bfa21eb986d3f0d0d52f775cece0cf6d41b01
brycebowman <Bryceabowman@gmail.com>
2021-06-23 21:55:10 -0500

    added icons for howitworksnew

--- src/components/SectionHowItWorksNew/SectionHowItWorksNew.js
+++ src/components/SectionHowItWorksNew/SectionHowItWorksNew.js
@@ -8,9 +8,9 @@ import { NamedLink } from '../../components';
 
 import css from './SectionHowItWorksNew.module.css';
 
-import nyImage from './images/ny-yogi.jpg';
-import laImage from './images/la-yogi.jpg';
-import sfImage from './images/sf-yogi.jpg';
+import cafeImage from './images/restaurant-icon.png';
+import workerImage from './images/female-food-worker.png';
+import signImage from './images/open-sign.png';
 
 class LocationImage extends Component {
   render() {
@@ -52,17 +52,17 @@ const SectionHowItWorksNew = props => {
       <div className={css.locations}>
         {locationLink(
           'New York',
-          nyImage,
+          cafeImage,
           '?address=New%20York%20City%2C%20New%20York%2C%20USA&bounds=40.917576401307%2C-73.7008392055224%2C40.477399%2C-74.2590879797556'
         )}
         {locationLink(
           'Los Angeles',
-          laImage,
+          workerImage,
           '?address=Los%20Angeles%2C%20California%2C%20USA&bounds=34.161440999758%2C-118.121305008073%2C33.9018913203336%2C-118.521456965901'
         )}
         {locationLink(
           'San Francisco',
-          sfImage,
+          signImage,
           '?address=San%20Francisco%2C%20California%2C%20USA&bounds=37.8324430069081%2C-122.354995082683%2C37.6044780500533%2C-122.517910874663'
         )}
       </div>
