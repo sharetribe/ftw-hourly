c873fc01bab9673f45bdb7cf6851ac8f3b445eb6
peytonhobson <peyton.hobson1@gmail.com>
2022-11-10 12:43:28 -0700

    Added times to availability table

--- src/components/ListingAvailabilityPanel/ListingAvailabilityPanel.js
+++ src/components/ListingAvailabilityPanel/ListingAvailabilityPanel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { intlShape, injectIntl, FormattedMessage } from '../../util/reactIntl';
@@ -9,18 +9,42 @@ import css from './ListingAvailabilityPanel.module.css';
 
 const DAYS_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+const createTimesArray = availabilityPlan => {
+  let result = new Array(7).fill('NA');
+
+  const entries = availabilityPlan.entries;
+
+  let i = 0;
+  let j = 0;
+
+  DAYS_OF_WEEK.forEach(day => {
+    if (day.toLowerCase().includes(entries[i] && entries[i].dayOfWeek)) {
+      result[i] = entries[i].startTime + ' - ' + entries[i].endTime;
+      i++;
+    }
+    j++;
+  });
+
+  return result;
+};
+
 const ListingAvailabilityPanel = props => {
   const { rootClassName, className, availabilityPlan } = props;
 
   const classes = classNames(rootClassName || css.root, className);
 
+  const formattedTimes = availabilityPlan && createTimesArray(availabilityPlan);
+
   return (
     <div className={classes}>
-      <div className={classes.recurringContainer}>
+      <div className={css.recurringContainer}>
         <h1 className={css.sectionTitle}>Recurring Availability</h1>
         <div className={css.recurringTable}>
-          {DAYS_OF_WEEK.map(day => (
-            <div className={css.day}>{day}</div>
+          {DAYS_OF_WEEK.map((day, index) => (
+            <Fragment>
+              <div className={css.day}>{day}</div>
+              <div className={css.times}>{formattedTimes[index]}</div>
+            </Fragment>
           ))}
         </div>
       </div>
