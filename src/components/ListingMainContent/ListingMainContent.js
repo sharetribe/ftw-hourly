994958be57f2635710831cf6d2e79a965627448a
peytonhobson <peyton.hobson1@gmail.com>
2022-11-09 19:12:54 -0700

    Added tabs to listing page

--- src/components/ListingMainContent/ListingMainContent.js
+++ src/components/ListingMainContent/ListingMainContent.js
@@ -5,6 +5,7 @@ import { intlShape, injectIntl, FormattedMessage } from '../../util/reactIntl';
 import { array, bool, func, node, object, oneOfType, shape, string } from 'prop-types';
 import classNames from 'classnames';
 import { convertFilterKeyToLabel } from '../../util/data';
+import { ButtonTabNavHorizontal } from '../TabNavHorizontal/TabNavHorizontal';
 
 import { types } from 'sharetribe-flex-sdk';
 const { Money } = types;
@@ -45,6 +46,25 @@ const priceData = (minPrice, maxPrice, intl) => {
   return {};
 };
 
+const tabLabel = (intl, tab) => {
+  let key = null;
+  if (tab === 'availability') {
+    key = 'AVAILABILITY';
+  } else if (tab === 'bio') {
+    key = 'BIO';
+  } else if (tab === 'services') {
+    key = 'SERVICES';
+  } else if (tab === 'qualifications') {
+    key = 'QUALIFICATIONS';
+  } else if (tab === 'safety') {
+    key = 'SAFETY';
+  } else if (tab === 'recommendations') {
+    key = 'RECOMMENDATIONS';
+  }
+
+  return intl.formatMessage({ id: key });
+};
+
 const experienceLevelConverter = new Map([
   ['novice', '1-2 years'],
   ['competent', '3-4 years'],
@@ -57,7 +77,41 @@ const convertExperienceToLabel = key => {
   return experienceLevelConverter.get(key);
 };
 
+const selectTab = e => {};
+
 const MIN_LENGTH_FOR_LONG_WORDS = 16;
+const tabs = [
+  {
+    text: 'availability',
+    selected: false,
+    onClick: selectTab,
+  },
+  {
+    text: 'bio',
+    selected: false,
+    onClick: selectTab,
+  },
+  {
+    text: 'services',
+    selected: false,
+    onClick: selectTab,
+  },
+  {
+    text: 'qualifications',
+    selected: false,
+    onClick: selectTab,
+  },
+  {
+    text: 'safety',
+    selected: false,
+    onClick: selectTab,
+  },
+  {
+    text: 'recommendations',
+    selected: false,
+    onClick: selectTab,
+  },
+];
 
 const ListingMainContent = props => {
   const { rootClassName, className, currentListing, intl } = props;
@@ -103,39 +157,44 @@ const ListingMainContent = props => {
 
   return (
     <div className={classes}>
-      <div className={css.primaryInfoContainer}>
-        <div>
-          <div className={css.primaryInfo}>
-            <span className={css.bold}>Pay rate: </span>
-            <span className={css.marketH2}>
-              &nbsp;
-              {formattedMinPrice}-{maxPrice / 100}
-            </span>
-            <span className={css.perUnit}>
-              &nbsp;
-              <FormattedMessage id={'CaregiverListingCard.perUnit'} />
-            </span>
-          </div>
-          <div className={css.primaryInfo}>
-            <span className={css.bold}>Experience: </span>
-            <span className={css.marketH2}>&nbsp;{convertedExperienceLevel}</span>
-          </div>
-          <div className={css.primaryInfo}>
-            <div className={css.marketH2}>
-              {richText(location, {
-                longWordMinLength: MIN_LENGTH_FOR_LONG_WORDS,
-                longWordClass: css.longWord,
-              })}
+      <div className={css.topSection}>
+        <div className={css.primaryInfoContainer}>
+          <div>
+            <div className={css.primaryInfo}>
+              <span className={css.bold}>Pay rate: </span>
+              <span className={css.marketH2}>
+                &nbsp;
+                {formattedMinPrice}-{maxPrice / 100}
+              </span>
+              <span className={css.perUnit}>
+                &nbsp;
+                <FormattedMessage id={'CaregiverListingCard.perUnit'} />
+              </span>
+            </div>
+            <div className={css.primaryInfo}>
+              <span className={css.bold}>Experience: </span>
+              <span className={css.marketH2}>&nbsp;{convertedExperienceLevel}</span>
+            </div>
+            <div className={css.primaryInfo}>
+              <div className={css.marketH2}>
+                {richText(location, {
+                  longWordMinLength: MIN_LENGTH_FOR_LONG_WORDS,
+                  longWordClass: css.longWord,
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={css.attributesContainer}>
-        <div className={css.attributesGrid}>
-          {convertedInfo.slice(0, 6).map(info => (
-            <div className={css.gridItem}>- {info}</div>
-          ))}
+        <div className={css.attributesContainer}>
+          <div className={css.attributesGrid}>
+            {convertedInfo.slice(0, 6).map(info => (
+              <div className={css.gridItem}>- {info}</div>
+            ))}
+          </div>
         </div>
+      </div>
+      <div className={css.tabsContainer}>
+        <ButtonTabNavHorizontal tabs={tabs} rootClassName={css.nav} tabRootClassName={css.tab} />
       </div>
     </div>
   );
