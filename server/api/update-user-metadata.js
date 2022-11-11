05efc3069599fd41c2a985cd6bd09661543df9bc
peytonhobson <peyton.hobson1@gmail.com>
2022-11-11 10:34:00 -0700

    Moved flex integration sdk out of dev dependencies

--- server/api/update-user-metadata.js
+++ server/api/update-user-metadata.js
@@ -1,29 +1,35 @@
 const { handleError, serialize } = require('../api-util/sdk');
-
 const flexIntegrationSdk = require('sharetribe-flex-integration-sdk');
+const log = require('../../src/util/log.js');
 
-const integrationSdk = flexIntegrationSdk.createInstance({
-  // These two env vars need to be set in the `.env` file.
-  clientId: process.env.FLEX_INTEGRATION_CLIENT_ID,
-  clientSecret: process.env.FLEX_INTEGRATION_CLIENT_SECRET,
+// const integrationSdk = flexIntegrationSdk.createInstance({
+//   // These two env vars need to be set in the `.env` file.
+//   clientId: process.env.FLEX_INTEGRATION_CLIENT_ID,
+//   clientSecret: process.env.FLEX_INTEGRATION_CLIENT_SECRET,
 
-  // Normally you can just skip setting the base URL and just use the
-  // default that the `createInstance` uses. We explicitly set it here
-  // for local testing and development.
-  baseUrl: process.env.FLEX_INTEGRATION_BASE_URL || 'https://flex-integ-api.sharetribe.com',
-});
-
+//   // Normally you can just skip setting the base URL and just use the
+//   // default that the `createInstance` uses. We explicitly set it here
+//   // for local testing and development.
+//   baseUrl: process.env.FLEX_INTEGRATION_BASE_URL || 'https://flex-integ-api.sharetribe.com',
+// });
+ß;
 module.exports = (req, res) => {
   const { email, metadata } = req.body;
 
-  integrationSdk.users
+  // const sdk = getSdk(req, res);
+
+  flexIntegrationSdk.users
     .show({ email })
     .then(userResponse => {
       const user = userResponse.data.data;
 
-      return integrationSdk.users.updateProfile(
+      // console.log(user);
+
+      // const trustedSdk = getTrustedSdk(req);
+
+      return flexIntegrationSdk.users.updateProfile(
         {
-          id: user.id,
+          id: user.id.uuid,
           metadata,
         },
         {
