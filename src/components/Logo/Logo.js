49bf96152748bd9151e6e5e0d219ecee92a2459a
tomwgf <tom@wellmadewebsite.co.uk>
2023-02-26 16:25:40 +0000

    Add files via upload

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import IconLogo from './IconLogo';
 import css from './Logo.module.css';
+import MobileLogoImage from './Local-Caddie-Booking-logo-2023-mobile.png';
+import DesktopLogoImage from './Local-Caddie-Booking-logo-2023-desktop.png';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
-  const mobileClasses = classNames(css.logoMobile, className);
-
-  // If you want to use image instead of svg as a logo you can use the following code.
-  // Also, remember to import the image as LogoImage here.
-  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+  const isMobile = format !== 'desktop';
+  const classes = classNames(className, { [css.logoMobile]: isMobile });
+  const logoImage = isMobile ? MobileLogoImage : DesktopLogoImage;
 
   return (
-    <IconLogo
-      className={format === 'desktop' ? className : mobileClasses}
-      format={format}
+    <img
+      className={classes}
+      src={logoImage}
+      alt={"Local Caddie Booking Scotland"}
       {...rest}
     />
   );
