0d4d86c262342e7ac77c2d248b370cb204c9d6da
tomwgf <tom@wellmadewebsite.co.uk>
2023-02-27 08:59:33 +0000

    Add files via upload

--- src/components/EditListingFeaturesPanel/EditListingFeaturesPanel.js
+++ src/components/EditListingFeaturesPanel/EditListingFeaturesPanel.js
@@ -10,7 +10,7 @@ import { ListingLink } from '../../components';
 
 import css from './EditListingFeaturesPanel.module.css';
 
-const FEATURES_NAME = 'yogaStyles';
+const FEATURES_NAME = 'caddieTypes';
 
 const EditListingFeaturesPanel = props => {
   const {
@@ -47,8 +47,8 @@ const EditListingFeaturesPanel = props => {
     <FormattedMessage id="EditListingFeaturesPanel.createListingTitle" />
   );
 
-  const yogaStyles = publicData && publicData.yogaStyles;
-  const initialValues = { yogaStyles };
+  const caddieTypes = publicData && publicData.caddieTypes;
+  const initialValues = { caddieTypes };
 
   return (
     <div className={classes}>
@@ -58,10 +58,10 @@ const EditListingFeaturesPanel = props => {
         name={FEATURES_NAME}
         initialValues={initialValues}
         onSubmit={values => {
-          const { yogaStyles = [] } = values;
+          const { caddieTypes = [] } = values;
 
           const updatedValues = {
-            publicData: { yogaStyles },
+            publicData: { caddieTypes },
           };
           onSubmit(updatedValues);
         }}
